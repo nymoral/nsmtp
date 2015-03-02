abb3e3e89d1a705ff15d03d6c10e6e347cdb77e2
Aivaras Saulius <aivaras.saulius@gmail.com>
2015-03-02 22:15:38 +0200

    Starting out

---
+++ nsmtp.py
@@ -0,0 +1,48 @@
+#! /bin/python3
+
+# Copyright (c) 2015, Aivaras Saulius
+# All rights reserved.
+# 
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions are met:
+# 
+# * Redistributions of source code must retain the above copyright notice, this
+# list of conditions and the following disclaimer.
+# 
+# * Redistributions in binary form must reproduce the above copyright notice,
+# this list of conditions and the following disclaimer in the documentation
+# and/or other materials provided with the distribution.
+# 
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+# AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+# IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+# DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+# FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+# DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+# SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+# CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+# OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+# OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+import ssl
+import socket
+
+
+def NewSMTPConnection(host, port, useTSL = True):
+    """ Try to make an smtp connection to host:port. If fails, returns None. Otherwise returns socket.socket (ssl.SSLSocket). """
+    cl = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+    if useTSL:
+        cl = ssl.wrap_socket(cl)
+
+    try:
+        cl.connect((host, int(port)))
+    except Exception:
+        return None
+
+    return cl
+
+if __name__ == "__main__":
+    with NewSMTPConnection("smtp.gmail.com", 465) as c:
+        pass
+
